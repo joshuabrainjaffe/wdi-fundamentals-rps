f0776f362f219a78f7d8fd05e74d5cb3784ec842
Joshua Brain Jaffe-Pinckney <newyshua@JoshuaBainJaffe.fios-router.home>
2015-09-24 13:20:15 -0400

    Please enter the commit message for your changes. Lines starting

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,23 +25,31 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+  
+    return move || randomplay();
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
+    if (playerMove === computerMove) {
+  winner='tie';
+    }
+    else if ((playerMove === 'rock' && computerMove === 'scissors') || (playerMove === 'paper' && computerMove === 'rock') || (playerMove === 'scissors' && computerMove === 'paper')){
+        winner='player';
+    }  
+    else {
+        winner='computer';
+    }   
+    
+return winner;
 }
 
 function playToFive() {
